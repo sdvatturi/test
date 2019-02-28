0a53664a15ff7b832b7ca635590271bf8f046626
sandeep <36277799+sdvatturi@users.noreply.github.com>
2019-02-28 14:16:14 -0500

    python

---
+++ input.py
@@ -0,0 +1,29 @@
+
+X="enter you \"Name\":"
+Y="enter you \"Age\":"
+#print("my name is "+ X +"")
+#print("I am "+ Y +" years old")
+
+#phrase= "sandeep vatturi"
+#print(phrase + " is cool")
+
+#phrase= "sandeep\"vatturi\""
+#print(phrase)
+
+#print(("my name is "+ X +"").upper())
+#print(("I am "+ Y +" years old").lower())
+
+#print(("my name is "+ X +"").isupper())
+#print((("I am "+ Y +" years old").lower()).islower())
+
+#print(phrase[8])
+#print(phrase.index("a"))
+
+#print(phrase.replace("sandeep","sandy"))
+
+#working with numbers.
+
+
+name= input(X)
+age= input(Y)
+print((("hello!!" + name + " you are " + age).upper()).replace("name[7]","K"))
